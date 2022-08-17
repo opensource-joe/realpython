eb02f4fa07fbab7528980f21f91feaa084287c04
opensource-joe <joe.castle@sas.com>
2022-08-16 23:58:15 -0400

    added enumerate and zip examples

---
+++ Exercises/enumerate.py
@@ -0,0 +1,4 @@
+players = ['Mike', 'Bob', 'Joe', 'Steve', 'Adam']
+
+for count, player in enumerate(players):
+    print(count, player)
---
+++ Exercises/zip.py
@@ -0,0 +1,5 @@
+countries = ['France', 'Tanzania', 'Canada']
+continents = ['Europe', 'Africa', 'North America']
+
+merged = list(zip(countries, continents))
+print(merged)
